a8609c7e9bf134c99f6c651a1879084ecb43ba6e
The Real Mi <miaolmedillo@gmail.com>
2023-09-18 16:24:45 -0400

    added productTag model fields

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,19 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
